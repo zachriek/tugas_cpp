e9eade737b080e9a30687646cec01dcfe0bde11e
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2023-10-01 08:31:29 +0700

    update

--- Pengusaha Donat/main.cpp
+++ Pengusaha Donat/main.cpp
@@ -19,23 +19,23 @@ string formatRupiah(string value, char thousandSep = '.')
 
 int main()
 {
-  cout << "Nama: Muhammad Zachrie Kurniawan" << endl;
-  cout << "NPM: 2315061113" << endl;
+  int jumlah_tepung = 20000,
+      jumlah_gula = 10000,
+      harga_donat = 6000,
+      harga_tepung = 200000,
+      harga_gula = 120000,
+      total_modal, jumlah_donat_1,
+      jumlah_donat_2, jumlah_donat,
+      total_harga_donat, keuntungan;
 
-  int jumlah_tepung = 20000;
-  int jumlah_gula = 10000;
+  total_modal = harga_tepung + harga_gula;
 
-  int harga_donat = 6000;
-  int harga_tepung = 200000;
-  int harga_gula = 120000;
-  int total_modal = harga_tepung + harga_gula;
+  jumlah_donat_1 = jumlah_tepung / 500 * 10;
+  jumlah_donat_2 = jumlah_gula / 100 * 10;
+  jumlah_donat = jumlah_donat_1 > jumlah_donat_2 ? jumlah_donat_2 : jumlah_donat_1;
 
-  int jumlah_donat_1 = jumlah_tepung / 500 * 10;
-  int jumlah_donat_2 = jumlah_gula / 100 * 10;
-  int jumlah_donat = jumlah_donat_1 > jumlah_donat_2 ? jumlah_donat_2 : jumlah_donat_1;
-
-  int total_harga_donat = jumlah_donat * harga_donat;
-  int keuntungan = total_harga_donat - total_modal;
+  total_harga_donat = jumlah_donat * harga_donat;
+  keuntungan = total_harga_donat - total_modal;
 
   cout << "Banyak donat gula yang bisa dibuat oleh bu Dona adalah " << jumlah_donat << " donat" << endl;
   cout << "Keuntungan yang diperoleh bu Dona adalah " << formatRupiah(to_string(keuntungan)) << endl;
